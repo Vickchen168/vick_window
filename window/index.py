5714a7789775ea81b1bf50f5c0e9c7268f8e0668
Vickchen168 <vickchen2017@gmail.com>
2024-05-28 11:32:54 +0800

    u

--- window/index.py
+++ window/index.py
@@ -1,5 +1,12 @@
-with open('names.txt', encoding='utf-8') as file: #file實體物件
-    content:str = file.read()
-names:list[str]=content.split()
-for name in names:
-    print(name)
+def get_names() -> list[str]:
+    with open('names.txt', encoding='utf-8') as file: #file實體物件
+        content:str = file.read()
+    names:list[str]=content.split()
+    return names
+
+# print(__name__) #內建值為 "__main__"
+# names:list[str] = get_names()
+
+if __name__ == '__main__':
+    names:list[str] = get_names()
+    print(names)
