1c79006f1bb25e44bd47fc5a7ad6ad146e973fe5
Vickchen168 <vickchen2017@gmail.com>
2024-07-09 11:53:46 +0800

    update

---
+++ 2024_07_09/main1.py
@@ -0,0 +1,21 @@
+from flask import Flask, url_for
+
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login abc'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    # print("run test_request_context")
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', name='vick',password='1234'))
+    print(url_for('profile', username='John Doe'))
