abed54aa45ebb9a1f4fe99408e3d92fc26d430c6
Vickchen168 <vickchen2017@gmail.com>
2024-07-31 15:11:03 +0800

    update

--- 2024_07_30_flask_dash/auth/0730_.sql
+++ 2024_07_30_flask_dash/auth/0730_.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS 使用者(
 	user_id SERIAL PRIMARY KEY,
 	姓名 VARCHAR(20),
 	性別 VARCHAR(20),
-	連絡電話 VARCHAR(20),
-	電子郵件 VARCHAR(40),
+	聯絡電話 VARCHAR(20),
+	電子郵件 VARCHAR(40) UNIQUE,
 	isGetEmail Bool,
 	出生年月日 VARCHAR(20),
 	自我介紹 VARCHAR(200),
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS 使用者(
 );
 
 
-INSERT INTO 使用者(姓名,電子郵件,密碼) 
-VALUES('robert','roberthsu@gmail.com','12345')
+INSERT INTO 使用者(姓名, 性別, 聯絡電話, 電子郵件, isgetemail,出生年月日, 自我介紹, 密碼, 連線密碼)
+	VALUES('robert','男','0933-333-333','roberthsu@gmail.com',true,'1911-01-01','About Me','12345','12345')
 
 
 SELECT *
@@ -23,3 +23,5 @@ FROM 使用者
 SELECT 密碼,姓名
 FROM 使用者
 where 電子郵件 = 'roberthsu@gmail.com'
+
+DROP table 使用者
