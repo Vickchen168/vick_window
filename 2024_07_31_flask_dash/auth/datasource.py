7b9147ae26346c28c4e16624d1335513eec9d515
Vickchen168 <vickchen2017@gmail.com>
2024-07-31 11:06:22 +0800

    update

---
+++ 2024_07_31_flask_dash/auth/datasource.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import psycopg2
+import os
+load_dotenv()
+
+def validateUser(email:str,password:str) -> tuple[bool,str]:
+    conn = psycopg2.connect(os.environ['POSTGRESQL_TOKEN'])
+    with conn:
+        with conn.cursor() as cursor:
+            sql ='''
+            SELECT 密碼,姓名
+            FROM 使用者
+            WHERE 電子郵件 = %s
+            '''
+            cursor.execute(sql,[email])
+            searchData:tuple[str, str] | None = cursor.fetchone()
+            if searchData:
+                database_password = searchData[0]
+                username = searchData[1]
+                return  password == database_password, username
+            else:
+                return False,""
+
+            
+    conn.close()
--- 2024_07_31_flask_dash/auth/main.py
+++ 2024_07_31_flask_dash/auth/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,session,redirect
 from flask_wtf import FlaskForm
 from wtforms import EmailField,PasswordField
 from wtforms.validators import DataRequired,Length
@@ -12,7 +12,7 @@ class LoginForm(FlaskForm):
 
 @auth_blueprint.route("/auth/",methods=['GET', 'POST'])
 @auth_blueprint.route("/auth/login",methods=['GET', 'POST'])
-def index():
+def index(email:str | None = None):
     form = LoginForm()
     if request.method == "POST":
         if form.validate_on_submit():
@@ -24,11 +24,20 @@ def index():
             print(f'password:{password}')
             is_ok, username = validateUser(email,password)
             if is_ok:
-                print(f"您好:{username}")
+                session['username'] = username
+                return redirect('/')
             else:
-                print(f'密碼錯誤')
+                form.email.errors.append("帳號或密碼錯誤")
+                form.email.data = ""
 
     else:
         print("這是第一次進入")
+        # if email is not None:
+        #     form.email.data = ""
     
     return render_template('/auth/login.html.jinja',form=form)
+
+@auth_blueprint.route("/auth/logout")
+def logout():
+    session.pop('username')
+    return redirect('/')
